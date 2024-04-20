3bbc5439dd27d7fb4178eb4b049cd844a6a21f4e
Lucille Jordaan <wiidlucille@gmail.com>
2024-04-20 15:52:23 +0200

    Adding helloWorld.py to branch issue-1

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,7 @@
 print("Git is Awesome!")
 
+credentials = input("Input your first and last names, please:\
+                'Peter Pan' \n ").capitalize()
+
+
+print(f"Hello {credentials} welcome to your GitHub! ")
